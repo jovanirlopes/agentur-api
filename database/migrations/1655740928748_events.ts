3e7fba0144dbe13795762f101750e56244bc0652
Jovanir Mateus de Almeida Lopes <jovanir.lopes@outlook.com>
2022-06-20 16:28:58 -0300

    Criado migration de categorias

---
+++ database/migrations/1655740928748_events.ts
@@ -0,0 +1,25 @@
+import BaseSchema from "@ioc:Adonis/Lucid/Schema";
+
+export default class extends BaseSchema {
+  protected tableName = "events";
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments("id");
+      table.text("title").notNullable();
+      table.text("info", "longtext").notNullable();
+      table.text("place").notNullable();
+      table.dateTime("startDate").notNullable();
+      table.dateTime("endDate").notNullable();
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp("created_at", { useTz: true });
+      table.timestamp("updated_at", { useTz: true });
+    });
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName);
+  }
+}
