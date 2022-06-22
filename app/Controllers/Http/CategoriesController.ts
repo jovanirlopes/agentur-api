6f1c49274bcf83a93fa212627ca4fdbbe449073d
Jovanir Mateus de Almeida Lopes <jovanir.lopes@outlook.com>
2022-06-22 11:22:34 -0300

    Adicionado metodo para listar categorias

--- app/Controllers/Http/CategoriesController.ts
+++ app/Controllers/Http/CategoriesController.ts
@@ -1,3 +1,26 @@
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class CategoriesController {}
+import Category from "App/Models/Category";
+
+export default class CategoriesController {
+  public async index({ response, request }) {
+    try {
+      const search = request.qs().search;
+      const page = request.qs().page;
+      if (search) {
+        const category = await Category.query().paginate(page);
+        return category;
+      } else {
+        const category = await Category.query()
+          .whereILike("category", search)
+          .paginate(page);
+        return category;
+      }
+    } catch (error) {
+      response.badRequest({
+        message: "Falha ao listar categorias",
+        error: error,
+      });
+    }
+  }
+}
