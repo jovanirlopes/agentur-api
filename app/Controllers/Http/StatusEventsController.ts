f5e03ce5bee40e048a935d087363e004c0ed05e9
Jovanir Mateus de Almeida Lopes <jovanir.lopes@outlook.com>
2022-06-23 14:29:42 -0300

    Adicionado metodo search e index

--- app/Controllers/Http/StatusEventsController.ts
+++ app/Controllers/Http/StatusEventsController.ts
@@ -1,3 +1,31 @@
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class StatusEventsController {}
+import StatusEvent from "App/Models/StatusEvent";
+
+export default class StatusEventsController {
+  public page: number;
+  public searchField: string | null;
+
+  public async index({ response, request }) {
+    this.page = request.qs().page || 1;
+    this.searchField = request.qs().searchField || null;
+    try {
+      return await this.search();
+    } catch (error) {
+      response.badRequest({
+        message: "Falha ao listar status dos eventos",
+        error: error,
+      });
+    }
+  }
+
+  private async search() {
+    if (this.searchField === null) {
+      return await StatusEvent.query().paginate(this.page);
+    } else {
+      return await StatusEvent.query()
+        .whereILike("name", this.searchField)
+        .paginate(this.page);
+    }
+  }
+}
