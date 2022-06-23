5d5fc9e36d2ec5b7152751b07fd71a2e183dd0e4
Jovanir Mateus de Almeida Lopes <jovanir.lopes@outlook.com>
2022-06-23 15:31:11 -0300

    Corrigido nome do controller errado

--- start/routes/statusEvent.ts
+++ start/routes/statusEvent.ts
@@ -1,9 +1,9 @@
 import Route from "@ioc:Adonis/Core/Route";
 
 Route.group(() => {
-  Route.get("/", "StatusEvent.index");
-  Route.post("/", "StatusEvent.store").middleware("auth");
-  Route.get("/:id", "StatusEvent.find");
-  Route.delete("/:id", "StatusEvent.delete").middleware("auth");
-  Route.put("/:id", "StatusEvent.updates");
+  Route.get("/", "StatusEventsController.index");
+  Route.post("/", "StatusEventsController.store").middleware("auth");
+  Route.get("/:id", "StatusEventsController.find");
+  Route.delete("/:id", "StatusEventsController.delete").middleware("auth");
+  Route.put("/:id", "StatusEventsController.updates");
 }).prefix("/status/events");
