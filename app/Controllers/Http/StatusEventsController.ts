168288637526ec9c2e70cc9bcc6f49ae5e9c37c0
Jovanir Mateus de Almeida Lopes <jovanir.lopes@outlook.com>
2022-06-23 15:09:26 -0300

    Desenvolvido metodos find store e delete

--- app/Controllers/Http/StatusEventsController.ts
+++ app/Controllers/Http/StatusEventsController.ts
@@ -13,9 +13,54 @@ export default class StatusEventsController {
       return await this.search();
     } catch (error) {
       response.badRequest({
-        message: "Falha ao listar status dos eventos",
+        message: "Falha ao listar os itens",
         error: error,
       });
+    }
+  }
+
+  public async find({ response, params }) {
+    try {
+      return await StatusEvent.findOrFail(params.id);
+    } catch (error) {
+      return response.badRequest({
+        message: "Falha ao buscar registro",
+        error: error,
+      });
+    }
+  }
+
+  public async store({ response, request }) {
+    try {
+      return await StatusEvent.create(request.all());
+    } catch (error) {
+      return response.badRequest({
+        message: "Falha ao inserir o registro",
+        error: error,
+      });
+    }
+  }
+
+  public async delete({ response, params }) {
+    try {
+      const statusEvent = await StatusEvent.findOrFail(params.id);
+      await statusEvent.delete();
+      return { message: "Deletado com sucesso", statusEvent };
+    } catch (error) {
+      return response.badRequest({
+        message: "Falha ao deletar o registro",
+        error: error,
+      });
+    }
+  }
+
+  public async update({ response, request, params }) {
+    try {
+      const statusEvent = await StatusEvent.findOrFail(params.id);
+      await statusEvent.merge(request.all()).save();
+      return statusEvent;
+    } catch (error) {
+      return response.badRequest({message: "Falha ao atualizar o registro"})
     }
   }
 
