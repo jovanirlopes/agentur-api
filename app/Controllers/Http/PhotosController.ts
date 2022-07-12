770cb77e2f50a5e3c10a9f98eedab2d39e8e20c6
Jovanir Mateus de Almeida Lopes <jovanir.lopes@outlook.com>
2022-07-12 16:08:20 -0300

    Upload de multiplas imagens

--- app/Controllers/Http/PhotosController.ts
+++ app/Controllers/Http/PhotosController.ts
@@ -8,19 +8,29 @@ export default class PhotosController {
   public s3 = Drive.use("s3");
 
   public path = Application.tmpPath("uploads");
-  public file;
   public fileName;
 
   public async store({ request, response, params }) {
     try {
-      this.file = request.file("photo");
-      await this.upload();
-      return await Photo.create({
-        fileName: this.file.fileName,
-        eventId: params.id,
-      });
+      let photos = request.files("photos");
+      let photosCreated: Photo[] = [];
+      for (let photo of photos) {
+        const name = await this.generateRandomName(photo.extname)
+        await this.upload(photo, name)
+        const created = await Photo.create({
+          fileName: name,
+          eventId: params.id
+        })
+        photosCreated.push(created)
+      }
+      return photosCreated
+      // await this.upload();
+      // return await Photo.create({
+      //   fileName: this.photos.fileName,
+      //   eventId: params.id,
+      // });
     } catch (error) {
-      response.badRequest(error);
+      response.badRequest(error.message);
     }
   }
 
@@ -50,24 +60,23 @@ export default class PhotosController {
     }
   }
 
-  public async upload() {
-    await this.generateRandomName();
-    await this.file.moveToDisk(
+  public async upload(file, name: string) {
+    await file.moveToDisk(
       "./",
       {
-        name: this.fileName,
-        contentType: this.file.type + "/" + this.file.extname,
+        name: name,
+        contentType: file.type + "/" + file.extname,
       },
       "s3"
     );
   }
 
-  public async generateRandomName() {
-    this.fileName = await (Math.random().toString(36).substring(2, 15) +
+  public async generateRandomName(ext: string) {
+    return await (Math.random().toString(36).substring(2, 15) +
       "-" +
       Date.now().toString() +
       "." +
-      this.file.extname);
+      ext);
   }
 
   public async find({ response, params }) {
